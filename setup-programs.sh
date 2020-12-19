dca9800327c4b8f5fd746941d2f673b8786f3fef
elchyan <elch.yan@yahoo.com>
2020-12-20 02:07:13 +0400

    Refactored setup-programs.sh

--- setup-programs.sh
+++ setup-programs.sh
@@ -1,47 +1,26 @@
 #!/bin/bash
 
-# Install iterm2
-brew install --cask iterm2 && echo "iterm2 installed"
+programs=(
+    "iterm2"
+    "keepassxc"
+    "sync"
+    "docker"
+    "visual-studio-code"
+    "intellij-idea"
+    "postman"
+    "brave-browser"
+    "google-chrome"
+    "signal"
+    "telegram"
+    "slack"
+    "protonvpn"
+    "qbittorrent"
+    "vlc"
+)
 
-# Install keepasxc
-brew install --cask keepassxc && echo "Keepassxc installed"
+for program in "${programs[@]}"
+do
+	brew install --cask $program && echo $program installed
+done
 
-# Install sync
-brew install --cask sync && echo "Sync installed"
-
-# Install docker
-brew install --cask docker && echo "Docker installed"
-
-# Install postman
-brew install --cask postman && echo "Postman installed"
-
-# Install vs-code
-brew install --cask visual-studio-code && echo "VS-Code installed"
 # cp -r ~/Sync/vscode/extensions ~/.vscode/extensions
-
-# Install IntelliJ
-brew install --cask intellij-idea && echo "IntelliJ installed"
-
-# Install Brave
-brew install --cask brave-browser && echo "Brave installed"
-
-# Install chrome
-brew install --cask google-chrome && echo "Chrome installed"
-
-# Install Signal
-brew install --cask signal && echo "Signal installed"
-
-# Install Telegram
-brew install --cask telegram && echo "Telegram installed"
-
-# Install Slack
-brew install --cask slack && echo "Slack installed"
-
-# Install ProtonVPN
-brew install --cask protonvpn && echo "ProtonVPN installed"
-
-# Install qbittorrent
-brew install --cask qbittorrent && echo "QbitTorrent installed"
-
-# Install vlc
-brew install --cask vlc && echo "VLC installed"
